f2ba2992dfa9027c1977d6a451bb2a028dad10ff
laurentpoirierfr@yahoo.fr <laurentpoirierfr@yahoo.fr>
2018-12-12 15:30:12 +0100

    Refactoring

--- spring-cloud-2.0.1/ms-frontend/src/main/java/app/Application.java
+++ spring-cloud-2.0.1/ms-frontend/src/main/java/app/Application.java
@@ -1,14 +1,11 @@
 package app;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.sleuth.zipkin2.ZipkinProperties;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
@@ -19,7 +16,7 @@ import org.springframework.context.annotation.Configuration;
 @EnableZuulProxy
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
